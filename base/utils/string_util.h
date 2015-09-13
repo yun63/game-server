a60441f9824ee6c14f6579ea60a9a896add83f54
yun63 <towardstheway@gmail.com>
2015-09-13 23:39:47 +0800

    字符串常用接口

--- base/utils/string_util.h
+++ base/utils/string_util.h
@@ -75,7 +75,7 @@ inline string join(const vector<string> &tokens,
 
 inline string ltrim(const string &str, const string &blank = " ")
 {
-    string::size_type pos = str.find_last_not_of(blank);
+    string::size_type pos = str.find_first_not_of(blank);
     if (pos == std::string::npos)
     {
         return str;
@@ -106,10 +106,60 @@ inline string trim(const string &str, const string &blank = " ")
     string::size_type pos2 = str.find_last_not_of(blank);
     if (pos2 == std::string::npos)
     {
+        return str.substr(pos);
     }
+
+    return str.substr(pos, pos2 - pos + 1);
 }
 
+inline string replace_all(string &str, const string &target, const string &replacement)
+{
+    string::size_type pos = str.find(target);
+    while (pos != string::npos)
+    {
+        str.replace(pos, target.length(), replacement);
+        pos = str.find(target);
+    }
 
+    return str;
+}
+
+// 另一个版本，效率可能不太高
+//inline string replace_all(const string &str, const string &target, const string &replacement)
+//{
+//    return join(split(str, target, false), replacement);
+//}
+
+inline string toupper(string &str)
+{
+    string s(str);
+    std::transform(s.begin(), s.end(), s.begin(), ::toupper);
+    return s;
+}
+
+inline string tolower(string &str)
+{
+    string s(str);
+    std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+    return s;
+}
+
+inline string read_from_file(const string &filepath)
+{
+    std::ifstream in(filepath.c_str());
+    string content((std::istreambuf_iterator<char>(in)),
+            (std::istreambuf_iterator<char>()));
+    in.close();
+
+    return content;
+}
+
+inline void write_to_file(const string &content, const string &filepath)
+{
+    std::ofstream out(filepath.c_str());
+    out << content;
+    out.close();
+}
 
 } // namespace basic_util
 
