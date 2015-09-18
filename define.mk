efe54ac2627e5a3b65f12ce48820ed142284f4e2
towardstheway@gmail.com <yunfei_lei@126.com>
2015-09-18 16:07:47 +0800

    公共文件添加xml

--- define.mk
+++ define.mk
@@ -12,11 +12,12 @@ SRCEXTS := .c .cc .cpp .cxx
 ## 指定C/C++编译器
 CC := gcc
 CXX := g++
+LINK := g++
 AR := ar
 RANLIB := ranlib
 
 ## 编译器选项
-CXXFLAGS := --std=c++11 -Wall
+CXXFLAGS := --std=c++11 -Wall -fPIC
 MYCFLAGS := -DDEBUG
 ARFLAGS := rcs
 
