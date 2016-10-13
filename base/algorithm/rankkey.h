5ac4a8d429187287962117999838ccf14d1881f4
towardstheway@gmail.com <yunfei_lei@126.com>
2016-10-13 18:56:39 +0800

    排行榜

---
+++ base/algorithm/rankkey.h
@@ -0,0 +1,25 @@
+// =====================================================================================
+// 
+//       Filename:  rankkey.h
+// 
+//    Description:  排序数据关键字
+// 
+//        Version:  1.0
+//        Created:  2016/10/13 18时54分04秒
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  lyf (), towardstheway@gmail.com
+//        Company:  
+// 
+// =====================================================================================
+
+
+#ifndef  RANKKEY_INC
+#define  RANKKEY_INC
+
+struct RankData {
+
+};
+
+#endif   // ----- #ifndef RANKKEY_INC  -----
