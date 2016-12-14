f8f53400d4eb5bfcf951ea232a77b67d095bc5ad
towardstheway@gmail.com <yunfei_lei@126.com>
2016-12-14 18:38:05 +0800

    atomic test

--- base/tests/atomic_unittest.cc
+++ base/tests/atomic_unittest.cc
@@ -21,8 +21,35 @@
 #include "thread/atomic.h"
 
 
-TEST(Atomic, all) {
+TEST(Atomic, AtomicInt32) {
     base::AtomicInt32 n;
     EXPECT_EQ(n.get(), 0);
+    EXPECT_EQ(n.get_and_add(1), 0);
+    EXPECT_EQ(n.get(), 1);
+    EXPECT_EQ(n.add_and_get(2), 3);
+    EXPECT_EQ(n.get(), 3);
+    EXPECT_EQ(n.inc_and_get(), 4);
+    EXPECT_EQ(n.get(), 4);
+    n.inc();
+    EXPECT_EQ(n.get(), 5);
+    EXPECT_EQ(n.add_and_get(-3), 2);
+    EXPECT_EQ(n.get_and_set(100), 2);
+    EXPECT_EQ(n.get(), 100);
+}
+
+TEST(Atomic, AtomicInt64) {
+    base::AtomicInt64 n;
+    EXPECT_EQ(n.get(), 0);
+    EXPECT_EQ(n.get_and_add(1), 0);
+    EXPECT_EQ(n.get(), 1);
+    EXPECT_EQ(n.add_and_get(2), 3);
+    EXPECT_EQ(n.get(), 3);
+    EXPECT_EQ(n.inc_and_get(), 4);
+    EXPECT_EQ(n.get(), 4);
+    n.inc();
+    EXPECT_EQ(n.get(), 5);
+    EXPECT_EQ(n.add_and_get(-3), 2);
+    EXPECT_EQ(n.get_and_set(100), 2);
+    EXPECT_EQ(n.get(), 100);
 }
 
