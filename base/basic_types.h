2ab8701b515b761291376fe1f42864f97364581b
towardstheway@gmail.com <yunfei_lei@126.com>
2016-12-30 18:44:05 +0800

    基础类型定义

--- base/basic_types.h
+++ base/basic_types.h
@@ -1,44 +1,52 @@
-// =====================================================================================
-// 
-//    @filename    :  basic_types.h
-// 
-//    @description :  基本数据类型定义
-// 
-// -------------------------------------------------------------------------------------
-//    @version     :  1.0
-//    @created     :  2013年11月12日 16时35分42秒
-//    @revision    :  none
-//    @compiler    :  g++
-// 
-//    @author      :  Lei Yunfei (leiyunfei), yunfei_lei@126.com
-//    @Company     :  
-// 
-// =====================================================================================
+/***************************************************************************
+ *
+ * Copyright © 2016 LT. All Rights Reserved.
+ *
+ ***************************************************************************/
 
-#ifndef  __BASIC_TYPES__H_
-#define  __BASIC_TYPES__H_
+/**
+ *
+ * @file: basic_types.h
+ *
+ * @breaf: 基础类型定义
+ *
+ * @author: Lei Yunfei(towardstheway@gmail.com)
+ *
+ * @create: 2016/12/30 18时41分46秒
+ *
+ **/
 
-#include <limits.h>
-#include <stddef.h>
+
+#ifndef _BASICTYPES_H_
+#define _BASICTYPES_H_
+
 #include <stdint.h>
+#include <sys/types.h>
 
-#include "macros.h"
+typedef int8_t              int8;
+typedef int16_t             int16;
+typedef int32_t             int32;
+typedef int64_t             int64;
 
-typedef int8_t      int8;
-typedef uint8_t     uint8;
-typedef int16_t     int16;
-typedef uint16_t    uint16;
-typedef int32_t     int32;
-typedef uint32_t    uint32;
+typedef uint8_t             uint8;
+typedef uint16_t            uint16;
+typedef uint32_t            uint32;
+typedef uint64_t            uint64;
 
-#if defined(__LP64__)
-typedef long int64;
-typedef unsigned long uint64;
-#else
-typedef long long int64;
-typedef unsigned long long uint64;
-#endif
+typedef unsigned char       btye;
 
-typedef unsigned char byte;
+const uint16 kuint16max = (   (uint16) 0xFFFF);
+const uint32 kuint32max = (   (uint32) 0xFFFFFFFF);
+const uint64 kuint64max = ( (((uint64) kuint32max) << 32) | kuint32max );
 
-#endif   // ----- #ifndef __BASIC_TYPES__H_ -----
+const  int8  kint8max   = (   (  int8) 0x7F);
+const  int16 kint16max  = (   ( int16) 0x7FFF);
+const  int32 kint32max  = (   ( int32) 0x7FFFFFFF);
+const  int64 kint64max =  ( ((( int64) kint32max) << 32) | kuint32max );
+
+const  int8  kint8min   = (   (  int8) 0x80);
+const  int16 kint16min  = (   ( int16) 0x8000);
+const  int32 kint32min  = (   ( int32) 0x80000000);
+const  int64 kint64min =  ( ((( int64) kint32min) << 32) | 0 );
+
+#endif  // _BASICTYPES_H_
