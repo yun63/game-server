0d7061df0b3502d34532e1a7b4137c111eeff314
towardstheway@gmail.com <yunfei_lei@126.com>
2017-02-27 18:17:44 +0800

    常量命名重新定义

--- base/basic_types.h
+++ base/basic_types.h
@@ -34,20 +34,20 @@ typedef uint16_t            uint16;
 typedef uint32_t            uint32;
 typedef uint64_t            uint64;
 
-typedef unsigned char       btye;
+typedef unsigned char       byte;
 
-const uint16 kuint16max = (   (uint16) 0xFFFF);
-const uint32 kuint32max = (   (uint32) 0xFFFFFFFF);
-const uint64 kuint64max = ( (((uint64) kuint32max) << 32) | kuint32max );
+const uint16 kUint16Max = (   (uint16) 0xFFFF);
+const uint32 kUint32Max = (   (uint32) 0xFFFFFFFF);
+const uint64 kUint64Max = ( (((uint64) kUint32Max) << 32) | kUint32Max );
 
-const  int8  kint8max   = (   (  int8) 0x7F);
-const  int16 kint16max  = (   ( int16) 0x7FFF);
-const  int32 kint32max  = (   ( int32) 0x7FFFFFFF);
-const  int64 kint64max =  ( ((( int64) kint32max) << 32) | kuint32max );
+const  int8  kInt8Max   = (   (  int8) 0x7F);
+const  int16 kInt16Max  = (   ( int16) 0x7FFF);
+const  int32 kInt32Max  = (   ( int32) 0x7FFFFFFF);
+const  int64 kInt64Max =  ( ((( int64) kInt32Max) << 32) | kUint32Max );
 
-const  int8  kint8min   = (   (  int8) 0x80);
-const  int16 kint16min  = (   ( int16) 0x8000);
-const  int32 kint32min  = (   ( int32) 0x80000000);
-const  int64 kint64min =  ( ((( int64) kint32min) << 32) | 0 );
+const  int8  kInt8Min   = (   (  int8) 0x80);
+const  int16 kInt16Min  = (   ( int16) 0x8000);
+const  int32 kInt32Min  = (   ( int32) 0x80000000);
+const  int64 kInt64Min =  ( ((( int64) kInt32Min) << 32) | 0 );
 
 #endif  // _BASICTYPES_H_
